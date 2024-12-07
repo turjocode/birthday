575dfe4a5cf82691745999ec71b400505e117304
Muhtasim Turjo <68634606+turjocode@users.noreply.github.com>
2024-12-07 18:35:07 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,47 @@
+document.getElementById('surpriseBtn').addEventListener('click', function () {
+    const surpriseText = document.getElementById('surpriseText');
+    surpriseText.textContent = " Prima, you're the brightest star in our lives! Wishing you a lifetime of happiness. ";
+    surpriseText.style.animation = "fadeIn 1.5s ease-in-out";
+});
+
+
+document.getElementById('surpriseBtn2').addEventListener('click', function () {
+    const surpriseText2 = document.getElementById('surpriseText2');
+    surpriseText2.textContent = " Happy Birthday, Prima. May you enjoy a long life and good health ";
+    surpriseText2.style.animation = "fadeIn 1.5s ease-in-out";
+});
+
+
+document.getElementById('surpriseBtn3').addEventListener('click', function () {
+    const surpriseText3 = document.getElementById('surpriseText3');
+    surpriseText3.textContent = " Happy birthday to my shining star. May your day be as remarkable as you are ";
+    surpriseText3.style.animation = "fadeIn 1.5s ease-in-out";
+});
+
+
+document.getElementById('surpriseBtn4').addEventListener('click', function () {
+    const surpriseText4 = document.getElementById('surpriseText4');
+    surpriseText4.textContent = " Happy Birthday, Prima! On your special day I wish you only smiles and joy ";
+    surpriseText4.style.animation = "fadeIn 1.5s ease-in-out";
+});
+
+
+document.getElementById('surpriseBtn5').addEventListener('click', function () {
+    const surpriseText5 = document.getElementById('surpriseText5');
+    surpriseText5.textContent = " Happy Birthday, Prima! May your day be filled with surprises and your year with joy ";
+    surpriseText5.style.animation = "fadeIn 1.5s ease-in-out";
+});
+
+
+document.getElementById('surpriseBtn6').addEventListener('click', function () {
+    const surpriseText6 = document.getElementById('surpriseText6');
+    surpriseText6.textContent = " Happy Birthday, Prima apu ";
+    surpriseText6.style.animation = "fadeIn 1.5s ease-in-out";
+});
+
+
+document.getElementById('surpriseBtn7').addEventListener('click', function () {
+    const surpriseText7 = document.getElementById('surpriseText7');
+    surpriseText7.textContent = " Happy Birthday, Sis! Hope your day is as wonderful as you are! ";
+    surpriseText7.style.animation = "fadeIn 1.5s ease-in-out";
+});
